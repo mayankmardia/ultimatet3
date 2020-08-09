a0528a2b2b5e59494cbbb12b2887f63202a7a7cf
Mayank Mardia <mardia.mayank@gmail.com>
2020-08-08 22:45:02 -0500

    Updates

---
+++ precache-manifest.cd7f99ddd6b3c5785d639ebab5eec7c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fd3dd26106f635eb272b0452b71899",
+    "url": "/ultimatet3/index.html"
+  },
+  {
+    "revision": "9a2e9812d71927d27028",
+    "url": "/ultimatet3/static/js/2.4add7ed3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ultimatet3/static/js/2.4add7ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad8928a1ac6dee6ddb98",
+    "url": "/ultimatet3/static/js/main.c4a9103f.chunk.js"
+  },
+  {
+    "revision": "067d31a5f3733992bafb",
+    "url": "/ultimatet3/static/js/runtime-main.5e584705.js"
+  }
+]);
